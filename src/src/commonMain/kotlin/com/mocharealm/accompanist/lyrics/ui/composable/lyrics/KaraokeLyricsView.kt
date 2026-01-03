f3874920f029d7e42cf0ebebf0b546a0ea04cdd6
Simon <Simon.Sleeeep@outlook.com>
2026-01-04 04:12:58 +0800

    feat: Improve performance

--- src/src/commonMain/kotlin/com/mocharealm/accompanist/lyrics/ui/composable/lyrics/KaraokeLyricsView.kt
+++ src/src/commonMain/kotlin/com/mocharealm/accompanist/lyrics/ui/composable/lyrics/KaraokeLyricsView.kt
@@ -33,10 +33,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -46,14 +45,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.util.fastRoundToInt
 import com.mocharealm.accompanist.lyrics.core.model.ISyncedLine
 import com.mocharealm.accompanist.lyrics.core.model.SyncedLyrics
 import com.mocharealm.accompanist.lyrics.core.model.karaoke.KaraokeAlignment
@@ -62,16 +61,9 @@ import com.mocharealm.accompanist.lyrics.core.model.synced.SyncedLine
 import com.mocharealm.accompanist.lyrics.ui.utils.isRtl
 import com.mocharealm.accompanist.lyrics.ui.utils.modifier.dynamicFadingEdge
 import com.mocharealm.gaze.capsule.ContinuousRoundedRectangle
-import kotlin.math.abs
-import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.ui.graphics.Brush
-import kotlinx.coroutines.withContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.isActive
-import androidx.compose.ui.text.style.TextDirection
-import com.mocharealm.accompanist.lyrics.ui.composable.lyrics.SyllableLayout
-import com.mocharealm.accompanist.lyrics.ui.composable.lyrics.measureSyllablesAndDetermineAnimation
+import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
@@ -94,12 +86,13 @@ fun KaraokeLyricsView(
     ),
     textColor: Color = Color.White,
     breathingDotsDefaults: KaraokeBreathingDotsDefaults = KaraokeBreathingDotsDefaults(),
-    verticalFadeBrush: Brush = Brush.verticalGradient(
-        0f to Color.White.copy(0f),
-        0.05f to Color.White,
-        0.6f to Color.White,
-        1f to Color.White.copy(0f)
-    ),
+//    TODO: pass it to dynamicFadeEdge
+//    verticalFadeBrush: Brush = Brush.verticalGradient(
+//        0f to Color.White.copy(0f),
+//        0.05f to Color.White,
+//        0.6f to Color.White,
+//        1f to Color.White.copy(0f)
+//    ),
     blendMode: BlendMode = BlendMode.Plus,
     useBlurEffect: Boolean = true,
     showDebugRectangles: Boolean = false
@@ -250,7 +243,7 @@ fun KaraokeLyricsView(
                 ) { index, line ->
                     val isCurrentFocusLine by rememberUpdatedState(index in allFocusedLineIndex)
 
-                    Column(modifier = Modifier.fillMaxWidth()) {
+                    Column(modifier = Modifier.fillMaxWidth().dynamicFadingEdge(listState, index)) {
                         val animDuration = 600
 
                         val previousLine = lyrics.lines.getOrNull(index - 1)
